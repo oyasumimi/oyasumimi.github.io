0438113bc77cf51e68b24c56a28c08bf55a0691d
oyasumimi <yasmindambros@hotmail.com>
2024-08-19 22:03:38 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 $( document ).ready( function( $ ) {
-    $( '.newTab a' ).click( function( event ){
+    $( '.c-site a' ).click( function( event ){
         event.preventDefault();
         window.open(( this ).href, '_blank');
     });
