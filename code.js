7e16b614d869bb865e1ad02e225afe96963542b7
oyasumimi <yasmindambros@hotmail.com>
2024-08-19 22:13:36 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,14 @@
-$(document).on('click', '.c-site a', function(e){ 
-    e.preventDefault(); 
-    var url = $(this).attr('href'); 
-    window.open(url, '_blank');
+function openInNewTab(url) {
+  window.open(url, '_blank');
+}
+document.addEventListener("DOMContentLoaded", function() {
+  var links = document.querySelectorAll('a');
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href && !links[i].href.startsWith(window.location.origin)) {
+      links[i].addEventListener('click', function(event) {
+        openInNewTab(this.href);
+        event.preventDefault();
+      });
+    }
+  }
 });
