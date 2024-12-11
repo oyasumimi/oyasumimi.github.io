dc6dede28170176b3c5b3eedb993fae19522a922
oyasumimi <yasmindambros@hotmail.com>
2024-12-11 16:05:58 -0300

    Update comment-widget.js

--- comment-widget.js
+++ comment-widget.js
@@ -47,7 +47,7 @@ const s_fixRarebitIndexPage = false; // If using Rarebit, change to true to make
 const s_wordFilterOn = false; // True for on, false for off
 const s_filterReplacement = '****'; // Change what filtered words are censored with (**** is the default)
 const s_filteredWords = [ // Add words to filter by putting them in quotes and separating with commas (ie. 'heck', 'dang')
-    'heck', 'dang'
+    'heck', 'nigger', 'nigga'
 ]
 
 // Text - Change what messages/text appear on the form and in the comments section (Mostly self explanatory)
